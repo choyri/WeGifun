090d6c2e34f24da74df32d3110b1c264f9d5625f
Chotow <i@choyri.com>
2017-07-07 17:40:52 +0800

    v0.2.1

--- utils/changelog.js
+++ utils/changelog.js
@@ -1,6 +1,10 @@
 let changelog = {
     zh_CN: [
         [
+            'v0.2.1 (20170707)',
+            '修复 未在教学周期内时课程不显示\n修复 存在连上的课程时导致课表无法显示'
+        ],
+        [
             'v0.2.0 (20170611)',
             '更新 优化了外观样式\n更新 重构了程序\n新增 校园卡服务，包括余额和消费记录查询\n新增 获取用户微信资料，显示头像和昵称\n新增 多语言，当前支持简体中文和英文'
         ],
@@ -10,6 +14,10 @@ let changelog = {
         ]
     ],
     en_US: [
+        [
+            'v0.2.1 (20170707)',
+            'FIX: Courses are not shown outside the teaching period\nFIX: The schedule can not be shown when continuous course existing'
+        ],
         [
             'v0.2.0 (20170611)',
             'UPDATE: Optimize the appearance style\nUPDATE: Refactor the program\nNEW: Campus Card Service, include the query of balance and consumer records\nNEW: Get the user\'s WeChat info to show avatar and nickname\nNEW: Multi-language, now supports Chinese(Simplified) and English'
